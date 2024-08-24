7b78fafe828b778e2ac0311e604927a9347d87a7
ghh <ghhayl520@gmail.com>
2024-08-24 20:29:48 +0800

    project创建

---
+++ project/javasepro/helloworld-app/src/com/gyy/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.gyy.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ project/javasepro/helloworld-app/src/com/gyy/speedkey/SpeedKeyDemo.java
@@ -0,0 +1,8 @@
+package com.gyy.speedkey;
+
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world2");
+    }
+}
