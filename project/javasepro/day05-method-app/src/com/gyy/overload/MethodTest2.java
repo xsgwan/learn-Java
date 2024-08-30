7feeed8160ef52938c938fd9054875e63152967e
ghh <ghhayl520@gmail.com>
2024-08-31 07:49:49 +0800

    方法重载

---
+++ project/javasepro/day05-method-app/src/com/gyy/overload/MethodTest2.java
@@ -0,0 +1,22 @@
+package com.gyy.overload;
+
+public class MethodTest2 {
+    public static void main(String[] args) {
+        // 目标:掌握方法重载的应用场景。
+        fire();
+        fire("岛国2");
+        fire("米国", 999);
+    }
+
+    public static void fire() {
+        fire("岛国");
+    }
+
+    public static void fire(String country) {
+        fire(country, 1);
+    }
+
+    public static void fire(String country, int number) {
+        System.out.println("发射了" + number + "枚武器给" + country);
+    }
+}
