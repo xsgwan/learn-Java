363b1332f93b9019404d68fec7771887a08a897e
ghh <ghhayl520@gmail.com>
2024-09-04 09:05:12 +0800

    成员变量、局部变量的区别

---
+++ project/javasepro/day07-oop-app/src/com/gyy/variable/Student.java
@@ -0,0 +1,12 @@
+package com.gyy.variable;
+
+public class Student {
+    String name;
+    double score;
+
+    public void aaa() {
+        double score = 98;
+        System.out.println(score);
+        System.out.println(this.score);
+    }
+}
