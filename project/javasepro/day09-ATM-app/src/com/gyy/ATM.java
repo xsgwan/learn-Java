0e160b38c83162f322e13dc9a0786a14be379506
ghh <ghhayl520@gmail.com>
2024-09-06 07:57:44 +0800

    ATM案例: 生成新卡号

--- project/javasepro/day09-ATM-app/src/com/gyy/ATM.java
+++ project/javasepro/day09-ATM-app/src/com/gyy/ATM.java
@@ -1,11 +1,12 @@
 package com.gyy;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class ATM {
-    private ArrayList<Acount> accounts = new ArrayList<>(); // []
+    private ArrayList<Acount> acounts = new ArrayList<>(); // []
     Scanner sc = new Scanner(System.in);
 
     /**
@@ -25,7 +26,7 @@ public class ATM {
                     break;
                 case "2":
                     // 用户开户
-                    createAccount();
+                    createAcount();
                     break;
                 default:
                     System.out.println("您输入的命令不存在!!");
@@ -36,16 +37,18 @@ public class ATM {
     /**
      * 完成用户开户操作
      */
-    private void createAccount() {
+    private void createAcount() {
         System.out.println("==系统开户操作==");
         // 1、创建一个账户对象，用于封装用户的开户信息
         Acount acount = new Acount();
 
         // 2、需要用户输入自己的开户信息，赋值给账户对象
+        // 输入名称
         System.out.println("请输入您的账户名称:");
         String name = sc.next();
         acount.setUserName(name);
 
+        // 输入性别
         while (true) {
             System.out.println("请输入您的性别:");
             char sex = sc.next().charAt(0);
@@ -57,6 +60,7 @@ public class ATM {
             }
         }
 
+        // 输入密码
         while (true) {
             System.out.println("请输入您的账户密码:");
             String password = sc.next();
@@ -70,15 +74,75 @@ public class ATM {
             }
         }
 
-        System.out.println("请输入您的每次提现额度");
+        // 输入限额
+        System.out.println("请输入您的每次提现额度:");
         double limit = sc.nextDouble();
         acount.setLimit(limit);
 
-        // 重点:我们需要为这个账户生成一个卡号（由系统自动生成。8位数字表示，不能与其他账户的卡号重复：会在下节课详细讲解）
+        // 重点:我们需要为这个账户生成一个卡号(由系统自动生成。8位数字表示，不能与其他账户的卡号重复)
+//        createCardId(acount);
+        String newCardId = createCardId();
+        acount.setCardId(newCardId);
 
         // 3、把这个账户对象，存入到账户集合中去
-        accounts.add(acount);
+        acounts.add(acount);
         System.out.println("恭喜您," + acount.getUserName() + "开户成功,您的卡号是:" + acount.getCardId());
+    }
+
+    /**
+     * 返回一个8位  数字的卡号，而且这个卡号不能与其他账户的卡号重复
+     */
+    private void createCardId(Acount acount) {
+        Random r = new Random();
+        StringBuilder cardId = new StringBuilder();
+        int num = 0;
+        for (int i = 1; i <= 8; i++) {
+            if (i == 1) {
+                num = r.nextInt(9) + 1;
+            } else {
+                num = r.nextInt(10);
+            }
+            cardId.append(num);
+        }
+
+        if (acounts.isEmpty()) {
+            acount.setCardId(cardId.toString());
+        }
+
+        for (int i = 0; i < acounts.size(); i++) {
+            if (cardId.toString().equals(acounts.get(i).getCardId())) {
+                break;
+            }
+            if (i == acounts.size() - 1) {
+                acount.setCardId(cardId.toString());
+            }
+        }
+    }
+
+    private String createCardId() {
+        String cardId = "";
+        Random r = new Random();
+        while (true) {
+            for (int i = 1; i <= 8; i++) {
+                int data = r.nextInt(10); // 0 - 9
+                cardId += data;
+            }
+            Acount acount = getAcountByCardId(cardId);
+            if (acount == null) {
+                // 说明cardId没有找到账户对象，因此cardId没有与其他账户的卡号重复，可以返回它做为一个新卡号
+                return cardId;
+            }
+        }
+    }
+
+    private Acount getAcountByCardId(String cardId) {
+        for (int i = 0; i < acounts.size(); i++) {
+            Acount acount = acounts.get(i);
+            if (acount.getCardId().equals(cardId)) {
+                return acount;
+            }
+        }
+        return null;
     }
 
     private void login() {
